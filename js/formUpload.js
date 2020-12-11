bf947980b8dd1b0f665276057be1896faf40a8db
Xwilarg <xwilarg@yahoo.fr>
2020-12-11 14:36:03 +0100

    Add body type and age

--- js/formUpload.js
+++ js/formUpload.js
@@ -6,7 +6,7 @@ document.getElementById("uploadInternal").addEventListener('change', () => {
         let json = JSON.parse(val.target.result);
         Array.prototype.slice.call(document.getElementsByTagName('input')).forEach(e => {
             let names = document.getElementsByName(e.name);
-            if (names.length > 0) {
+            if (names.length > 0 && json[e.name] !== undefined) {
                 names[0].value = json[e.name];
                 let container = document.getElementById(e.name + "Container");
                 if (names[0].value.length > 0 && container !== null) {
@@ -41,6 +41,8 @@ document.getElementById("uploadInternal").addEventListener('change', () => {
                 }
             }
         });
+        calculateBMI();
+        calculateAge();
     };
     reader.readAsText(file);
 });
@@ -51,4 +53,31 @@ document.getElementById("gender").addEventListener("change", () => {
     } else {
         document.getElementById("genderContainer").classList.add("hidden");
     }
-})
+});
+
+function calculateBMI() {
+    let h = document.getElementById("height").value / 100;
+    let w = document.getElementById("weight").value;
+
+    let bmi = w / (h * h);
+    document.getElementById("bmi").innerHTML = bmi.toFixed(1);
+    if (bmi !== 0) {
+        let bmiText;
+        if (bmi < 18.5) bmiText = "Underweight (< 18.5)";
+        else if (bmi < 25) bmiText = "Normal (18.5 - 24.9)";
+        else if (bmi < 30) bmiText = "Overweight (25.0 - 29.9)";
+        else if (bmi < 35) bmiText = "Obese (30.0 - 34.9)";
+        else bmiText = "Extremly Obese (> 35.0)";
+        document.getElementById("bmiText").innerHTML = bmiText;
+    }
+}
+
+function calculateAge() {
+    document.getElementById("age").innerHTML = new Date(Date.now() - Date.parse(document.getElementById("birthdate").value)).getUTCFullYear() - 1970;
+}
+
+document.getElementById("height").addEventListener("change", calculateBMI);
+document.getElementById("weight").addEventListener("change", calculateBMI);
+
+
+document.getElementById("birthdate").addEventListener("change", calculateAge());
