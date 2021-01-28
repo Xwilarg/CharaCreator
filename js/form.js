cb2b6820c19b456d4d3f4b050db51d42c7bbedc5
Xwilarg <xwilarg@yahoo.fr>
2021-01-28 14:28:29 +0100

    Clean code

--- js/form.js
+++ js/form.js
@@ -21,6 +21,7 @@ function save() {
     download("character.json", JSON.stringify(json));
 }
 
+// Download a file to the user's computer
 function download(filename, content) {
     let file = document.createElement('a');
     file.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(content));
@@ -31,6 +32,64 @@ function download(filename, content) {
     document.body.removeChild(file);
 }
 
+// Called when we modify the value in a <select> and select "other", we need to display the "Other" input field
+function selectChange(id) {
+    if (document.getElementById(id).value === "other") {
+        document.getElementById(id + "Container").classList.remove("hidden");
+    } else {
+        document.getElementById(id + "Container").classList.add("hidden");
+    }
+}
+
+// We calculate the BMI (Body Mass Index)
+function calculateBMI() {
+    let h = document.getElementById("height").value / 100;
+    let w = document.getElementById("weight").value;
+
+    let bmi = w / (h * h);
+    document.getElementById("bmi").innerHTML = bmi.toFixed(1);
+    if (bmi !== 0) {
+        let bmiText;
+        if (bmi < 18.5) bmiText = "Underweight (< 18.5)";
+        else if (bmi < 25) bmiText = "Normal (18.5 - 24.9)";
+        else if (bmi < 30) bmiText = "Overweight (25.0 - 29.9)";
+        else if (bmi < 35) bmiText = "Obese (30.0 - 34.9)";
+        else bmiText = "Extremly Obese (> 35.0)";
+        document.getElementById("bmiText").innerHTML = bmiText;
+    }
+}
+
+// Age is just current date - birthday date
+function calculateAge() {
+    document.getElementById("age").innerHTML = new Date(Date.now() - Date.parse(document.getElementById("birthdate").value)).getUTCFullYear() - 1970;
+}
+
+// Button to upload a file, call "uploadInternal" (see below)
 function upload() {
     document.getElementById("uploadInternal").click();
 }
+
+window.onload = function() {
+    // Upload a file from the user computer and put all the values on the page
+    document.getElementById("uploadInternal").addEventListener('change', () => {
+        let file = document.getElementById("uploadInternal").files[0];
+        if (!file) return;
+        let reader = new FileReader();
+        reader.onload = function(val) {
+            let json = JSON.parse(val.target.result);
+            loadCurrent(json);
+        };
+        reader.readAsText(file);
+    });
+    // Add check when a select is changed
+    Array.prototype.slice.call(document.getElementsByTagName("select")).forEach(elem => {
+        elem.addEventListener("change", function (e) {
+            selectChange(e.originalTarget.id);
+        });
+    });
+    // We recalculate the BMI when the user change the height or weight
+    document.getElementById("height").addEventListener("change", calculateBMI);
+    document.getElementById("weight").addEventListener("change", calculateBMI);
+    // We recalculate the age when the user change the birthday
+    document.getElementById("birthdate").addEventListener("change", calculateAge());
+};
--- js/saveLoad.js
+++ js/saveLoad.js
@@ -1,10 +1,12 @@
 function saveCurrent() {
     let json = new Object();
+    // We go through all inputs to see if we need to save thel
     Array.prototype.slice.call(document.getElementsByTagName('input')).forEach(e => {
         if (!e.name.endsWith("Other") && e.name.length > 0) { // We ignore "other" input field since they are associated with a select
             json[e.name] = e.value;
         }
     });
+    // Then we go through top down menus
     Array.prototype.slice.call(document.getElementsByTagName('select')).forEach(e => {
         if (e.value === "other") { // "Other" answer, we check the input field
             json[e.name] = document.getElementsByName(e.name + "Other")[0].value;
@@ -16,24 +18,28 @@ function saveCurrent() {
 }
 
 function loadCurrent(json) {
+    // For each input in the document, we see if we have an element with the same name in our JSON
     Array.prototype.slice.call(document.getElementsByTagName('input')).forEach(e => {
         let names = document.getElementsByName(e.name);
         if (names.length > 0 && json[e.name] !== undefined) {
             names[0].value = json[e.name];
             let container = document.getElementById(e.name + "Container");
+            // If the input is originally hidden in the form, we remove the "hidden" attribute
             if (names[0].value.length > 0 && container !== null) {
                 container.classList.remove("hidden");
+                container.classList.add("wasHidden"); // We keep track of attributes that used to be hidden
             }
         }
     });
+    // Then we go through all select in the document
     Array.prototype.slice.call(document.getElementsByTagName('select')).forEach(e => {
         let arr = [];
         Array.apply(null, e.options).forEach(e => {
             arr.push(e.value)
         });
+        // We check for each elements in the select, in our JSON...
         if (json[e.name] === undefined) {
-
-        } else if (!arr.includes(json[e.name])) {
+        } else if (!arr.includes(json[e.name])) { // If the element in our JSON isn't in the select, that means we must put it in the "other" option
             console.log(e.name);
             let selectNames = document.getElementsByName(e.name + "Other");
             let names = document.getElementsByName(e.name);
@@ -42,13 +48,15 @@ function loadCurrent(json) {
             }
             if (selectNames.length > 0) {
                 selectNames[0].value = json[e.name];
+                // We then fill the "other" input
                 let container = document.getElementById(e.name + "Container");
                 if (container !== null) {
                     container.classList.remove("hidden");
+                    container.classList.add("wasHidden");
                 }
             }
         }
-        else {
+        else { // If the element is in the JSON, we just put the right value
             let names = document.getElementsByName(e.name);
             if (names.length > 0) {
                 names[0].value = json[e.name];
