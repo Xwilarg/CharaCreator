dcfbcc0d11a7f079688a8e0f6b45c30c7190feed
Xwilarg <xwilarg@yahoo.fr>
2021-01-30 04:55:54 +0100

    Fix issue with comparator table

--- js/comparator.js
+++ js/comparator.js
@@ -1,18 +1,25 @@
+let comparatorTable = undefined;
+
 function loadComparator() {
+    if (comparatorTable !== undefined) { // Remove previous instance of the table if existing
+        comparatorTable.destroy();
+    }
+
     let data = [];
     for (const [_, json] of Object.entries(allProfiles)) {
-        // TABLE ELEMENTS: Name, Age
+        // TABLE ELEMENTS: Name, Age, BMI
         let arr = [];
         arr.push(json.lastName + " " + json.firstName);
         arr.push(new Date(Date.now() - Date.parse(json.birthdate)).getUTCFullYear() - 1970);
+        arr.push((json.weight / ((json.height / 100) * (json.height / 100))).toFixed(1));
         data.push(arr);
     }
 
-    new Handsontable(document.getElementById("comparationTable"), {
+    comparatorTable = new Handsontable(document.getElementById("comparationTable"), {
         licenseKey: "non-commercial-and-evaluation",
         data: data,
         rowHeaders: true, // Row headers (1, 2, 3...)
-        colHeaders: ["Name", "Age"], // Column headers
+        colHeaders: ["Name", "Age", "BMI"], // Column headers
         editor: false, // Can't edit cells
         dropdownMenu: true, // Drop down menu to display filters
         filters: true, // Enable filters
