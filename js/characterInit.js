345f8b5fadc3884589072707455c8f42167ad006
Xwilarg <xwilarg@yahoo.fr>
2021-03-06 14:26:20 +0100

    Add personnality comparator

--- js/characterInit.js
+++ js/characterInit.js
@@ -1,4 +1,5 @@
 window.onload = function() {
+    comparatorCtor();
     formCtor();
     groupByCtor();
     personnalityCtor();
--- js/comparator.js
+++ js/comparator.js
@@ -2,13 +2,24 @@ let comparatorTable = undefined;
 
 let last = "main";
 
+function comparatorCtor() {
+    let str = "";
+    for (const [_, trait] of Object.entries(personnalityTraits)) {
+        str += "<h4>" + trait + "</h3>";
+        str += '<div id="comparator_' + trait + '"></div>'
+    }
+    document.getElementById("personnalityComparationTable").innerHTML = str;
+}
+
 function loadComparator() {
     displayComparator(last);
 
 }
 
 function displayComparator(id) {
+    last = id;
     document.getElementById("buttonGeneralComparator").classList.remove("selected");
+    document.getElementById("buttonPersonnalityComparator").classList.remove("selected");
     document.getElementById("buttonHobbyComparator").classList.remove("selected");
     document.getElementById("buttonFetishComparator").classList.remove("selected");
     document.getElementById("buttonDiseaseComparator").classList.remove("selected");
@@ -16,42 +27,87 @@ function displayComparator(id) {
 
     document.getElementById("generalComparationTable").classList.add("hidden");
     document.getElementById("arrayComparationTable").classList.add("hidden");
+    document.getElementById("personnalityComparationTable").classList.add("hidden");
 
     switch (id) {
         case "main":
             document.getElementById("buttonGeneralComparator").classList.add("selected");
             document.getElementById("generalComparationTable").classList.remove("hidden");
-            last = "main";
             loadComparatorMain();
+            break;
+
+        case "personnality":
+            document.getElementById("buttonPersonnalityComparator").classList.add("selected");
+            document.getElementById("personnalityComparationTable").classList.remove("hidden");
+            loadComparatorPersonnality();
             break;
 
         case "hobby":
             document.getElementById("buttonHobbyComparator").classList.add("selected");
             document.getElementById("arrayComparationTable").classList.remove("hidden");
-            last = "hobby";
             loadComparatorInternal("likesArray", "likeNamePart");
             break;
 
         case "fetish":
             document.getElementById("buttonFetishComparator").classList.add("selected");
             document.getElementById("arrayComparationTable").classList.remove("hidden");
-            last = "fetish";
             loadComparatorInternal("fetishesArray", "fetishNamePart");
             break;
 
         case "disease":
             document.getElementById("buttonDiseaseComparator").classList.add("selected");
             document.getElementById("arrayComparationTable").classList.remove("hidden");
-            last = "disease";
             loadComparatorInternal("diseasesArray", "diseaseNamePart");
             break;
 
         case "phobia":
             document.getElementById("buttonPhobiaComparator").classList.add("selected");
             document.getElementById("arrayComparationTable").classList.remove("hidden");
-            last = "phobia";
             loadComparatorInternal("phobiasArray", "phobiaNamePart");
             break;
+    }
+}
+
+let personnalityTables = [];
+function loadComparatorPersonnality() {
+    for (const [_, table] of Object.entries(personnalityTables)) {
+        table.destroy();
+    }
+    personnalityTables = [];
+
+    let profiles = [];
+    for (const [_, json] of Object.entries(allProfiles)) {
+        if (isPersonnalitySet(json)) {
+            profiles.push(json);
+        }
+    }
+
+    for (const [_, trait] of Object.entries(personnalityTraits)) {
+        let data = [];
+
+        for (const [_, json] of Object.entries(profiles)) {
+            let arr = [];
+            arr.push(getName(json));
+            arr.push(json[trait]);
+            arr.push(json[trait + "_description"]);
+            data.push(arr);
+        }
+
+        if (data.length === 0) {
+            data.push(["", "", "", "", "", "", ""]);
+        }
+
+        personnalityTables.push(new Handsontable(document.getElementById("comparator_" + trait), {
+            licenseKey: "non-commercial-and-evaluation",
+            data: data,
+            rowHeaders: true, // Row headers (1, 2, 3...)
+            colHeaders: ["Name", "Score", "Description"], // Column headers
+            editor: false, // Can't edit cells
+            dropdownMenu: true, // Drop down menu to display filters
+            filters: true, // Enable filters
+            dropdownMenu: ['filter_by_condition', 'filter_by_value', 'filter_action_bar'], // Filter conditions
+            columnSorting: true // Click a column to sort it
+        }));
     }
 }
 
